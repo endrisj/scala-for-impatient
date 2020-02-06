910aa855fb3149d578c4695350f32a0ba9f2a006
Andrius Jukna <aj@platform-lunar.com>
2020-02-06 16:22:33 +0200

    Chapters 5 in progress

--- src/main/scala/chapter/c05/ExerciseCh05.scala
+++ src/main/scala/chapter/c05/ExerciseCh05.scala
@@ -1,12 +1,16 @@
 package chapter.c05
 
 object ExerciseCh05 extends App {
-  e02
+  e04
 
   def e01: Unit = {
     class Counter {
       private var value = Int.MaxValue
-      def increment() { if (value == Int.MaxValue) value = 0 else value += 1 }
+
+      def increment() {
+        if (value == Int.MaxValue) value = 0 else value += 1
+      }
+
       def current = value
     }
     val counter = new Counter
@@ -18,6 +22,48 @@ object ExerciseCh05 extends App {
   }
 
   def e02: Unit = {
+    class BankAccount {
+      private var balance = 0.0
+
+      def current = balance
+
+      def deposit(amount: Double): Unit = {
+        balance += amount
+      }
+
+      def withdraw(amount: Double): Unit = {
+        balance -= amount
+      }
+    }
+
+    val account = new BankAccount
+    account.deposit(100)
+    println(account.current)
+
+    account.withdraw(44.57)
+    println(account.current)
+  }
+
+  def e03: Unit = {
+    class Time(val hours: Int, val minutes: Int) {
+      require(hours >= 0 && hours < 24, s"Hours must be between 0 and 23. Hour `${hours}` is invalid.")
+      require(minutes >= 0 && minutes < 60, s"Minutes must be between 0 and 59. Minute `${minutes}` is invalid.")
+
+      def before(other: Time): Boolean = {
+        other.hours > hours || (other.hours == hours && other.minutes > minutes)
+      }
+    }
+
+    val time1230 = new Time(12, 30)
+    val time1445 = new Time(14, 45)
+    println(time1230.before(time1445))
+    println(time1230.before(time1230))
+    println(time1445.before(time1230))
+
+    val invalidTime = new Time(25, 45)
+  }
+
+  def e04: Unit ={
 
   }
 }
